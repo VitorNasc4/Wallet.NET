0b663a25b77f8ea8c4fcfe9b5e248edc57462eba
vitor <vitornascimento@321@hotmail.com>
2024-07-26 09:36:33 -0300

    chore: configurando entities

---
+++ Wallet.NET/Models/Stock.cs
@@ -0,0 +1,26 @@
+﻿namespace Wallet.NET.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Ticker { get; set; } = null!;
+        public string Exchange { get; set; } = null!;
+        public static bool IsValidExchange(string exchange)
+        {
+            if (string.IsNullOrEmpty(exchange))
+            {
+                return false;
+            }
+
+            var validExchanges = new List<string>() { ExchangeTypes.BOVESPA, ExchangeTypes.NASDAQ };
+
+            return validExchanges.Contains(exchange.ToUpperInvariant());
+        }
+    }
+}
+
+public static class ExchangeTypes
+{
+    public static string BOVESPA = "BOVESPA";
+    public static string NASDAQ = "NASDAQ";
+}
---
+++ Wallet.NET/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Wallet.NET.Data;
+using Wallet.NET.Models;
+
+namespace Wallet.NET.Models
+{
+    public class User : ApplicationUser
+    {
+        public string Name { get; set; } = null!;
+        public ICollection<Stock> Stocks { get; set; } = new List<Stock>();
+    }
+}
