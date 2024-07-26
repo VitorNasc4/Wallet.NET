4ac9c9d6200cd1988552bdfe5d2b1a758467d191
vitor <vitornascimento@321@hotmail.com>
2024-07-26 08:58:50 -0300

    Initial commit

---
+++ Wallet.NET/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Wallet.NET.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
