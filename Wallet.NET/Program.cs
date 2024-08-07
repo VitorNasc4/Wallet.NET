8a6e2562622c5a3e60834d866b4d51348fc0a1f0
vitor <vitornascimento@321@hotmail.com>
2024-08-07 09:36:34 -0300

    chore: configurações de novos serviços

--- Wallet.NET/Program.cs
+++ Wallet.NET/Program.cs
@@ -1,13 +1,19 @@
+using Hangfire;
+using Hangfire.Redis.StackExchange;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
+using StackExchange.Redis;
 using Wallet.NET.Components;
 using Wallet.NET.Components.Account;
 using Wallet.NET.Data;
 using Wallet.NET.Repositories.Stocks;
+using Wallet.NET.Services.Email;
 using Wallet.NET.Services.Indices;
+using Wallet.NET.Services.Monitor;
 using Wallet.NET.Services.News;
+using Wallet.NET.Services.Report;
 using Wallet.NET.Services.Stocks;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,6 +35,8 @@ builder.Services.AddScoped<IIndexService, IndexService>();
 builder.Services.AddScoped<INewsService, NewsService>();
 builder.Services.AddScoped<IStockService, StockService>();
 builder.Services.AddScoped<ICryptoService, CryptoService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IReportService, ReportService>();
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 builder.Services.AddAuthentication(options =>
@@ -51,6 +59,16 @@ builder.Services.AddIdentityCore<ApplicationUser>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+builder.Services.AddHangfire(options =>
+{
+    var connectionString = builder.Configuration.GetValue<string>("RedisConnection");
+    var redis = ConnectionMultiplexer.Connect(connectionString!);
+
+    options.UseRedisStorage(redis, options: new RedisStorageOptions { Prefix = $"HANG_FIRE"});
+});
+builder.Services.AddHangfireServer();
+builder.Services.AddHostedService<MonitorService>();
 
 var app = builder.Build();
 
@@ -77,4 +95,5 @@ app.MapRazorComponents<App>()
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
 
+app.MapHangfireDashboard("/hangfire");
 app.Run();
